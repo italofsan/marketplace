218f18ed7e1ce31da3dd2d481ab39f1d9b5fd23e
Italo Santos <italofsan@outlook.com>
2025-10-12 22:35:41 -0300

    Configurando rotas públicas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,5 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import { router } from 'expo-router'
+import { Redirect } from 'expo-router'
 
 export default function App() {
-  return (
-    <View>
-      <TouchableOpacity
-        onPress={() => {
-          router.push('/login')
-        }}
-      >
-        <Text className='text-blue-base'>Login</Text>
-      </TouchableOpacity>
-    </View>
-  )
+  return <Redirect href='/login' />
 }
--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { router } from 'expo-router'
 
 export default function Login() {
   return (
-    <View>
+    <View className='flex-1 items-center justify-center'>
       <Text>Login</Text>
+      <TouchableOpacity
+        onPress={() => {
+          router.push('/register')
+        }}
+      >
+        <Text className='text-blue-base'>Registro</Text>
+      </TouchableOpacity>
     </View>
   )
 }
