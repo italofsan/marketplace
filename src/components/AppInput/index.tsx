d7c8be9770d40cb5e2911a8162aae3805f9160da
Italo Santos <italofsan@outlook.com>
2025-10-22 06:03:29 -0300

    Configurando estilo no input global

--- src/components/AppInput/index.tsx
+++ src/components/AppInput/index.tsx
@@ -1,20 +1,41 @@
-import { Pressable, TextInput, TouchableOpacity, View } from 'react-native'
+import {
+  Pressable,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Text,
+  TextInputProps,
+} from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 
-import { appInputVarants } from './input.variants'
+import { appInputVarants, AppInputVariants } from './input.variants'
 
-export const AppInput = () => {
+export interface AppInputProps extends TextInputProps, AppInputVariants {
+  label?: string
+  leftIcon?: keyof typeof Ionicons.glyphMap
+  rightIcon?: keyof typeof Ionicons.glyphMap
+  containerClassName?: string
+  mask?: (value: string) => string
+}
+
+export const AppInput = ({
+  label,
+  leftIcon,
+  rightIcon,
+  containerClassName,
+  ...rest
+}: AppInputProps) => {
   const styles = appInputVarants({})
 
   return (
-    <View>
-      <Pressable>
-        <Ionicons name='person' />
-
-        <TextInput />
+    <View className={styles.container({ className: containerClassName })}>
+      <Text className={styles.label()}>{label}</Text>
+      <Pressable className={styles.wrapper()}>
+        {leftIcon && <Ionicons name={leftIcon} size={22} />}
+        <TextInput className={styles.input()} {...rest} />
 
         <TouchableOpacity>
-          <Ionicons name='eye-off-outline' />
+          {rightIcon && <Ionicons name={rightIcon} size={22} />}
         </TouchableOpacity>
       </Pressable>
     </View>
