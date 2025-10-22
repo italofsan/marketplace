3441022f3e6655677962dfb90e088d51c3d901b9
Italo Santos <italofsan@outlook.com>
2025-10-22 06:42:23 -0300

    Criando ViewModel do Input global

--- src/components/AppInput/index.tsx
+++ src/components/AppInput/index.tsx
@@ -9,6 +9,7 @@ import {
 import { Ionicons } from '@expo/vector-icons'
 
 import { appInputVarants, AppInputVariants } from './input.variants'
+import { useAppInputViewModel } from './useAppInputViewModel'
 
 export interface AppInputProps extends TextInputProps, AppInputVariants {
   label?: string
@@ -16,6 +17,7 @@ export interface AppInputProps extends TextInputProps, AppInputVariants {
   rightIcon?: keyof typeof Ionicons.glyphMap
   containerClassName?: string
   mask?: (value: string) => string
+  error?: string
 }
 
 export const AppInput = ({
@@ -23,19 +25,45 @@ export const AppInput = ({
   leftIcon,
   rightIcon,
   containerClassName,
+  value,
+  isError,
+  secureTextEntry,
+  onBlur,
+  onFocus,
+  onChangeText,
+  error,
+  mask,
+  isDisable,
   ...rest
 }: AppInputProps) => {
+  const {
+    getIconColor,
+    handleWrapperPress,
+    handlePasswordToggle,
+    handleFocus,
+    handleBlur,
+  } = useAppInputViewModel({
+    error,
+    onBlur,
+    onFocus,
+    isError: !!error,
+    mask,
+    onChangeText,
+    isDisable,
+    secureTextEntry,
+    value,
+  })
   const styles = appInputVarants({})
 
   return (
     <View className={styles.container({ className: containerClassName })}>
       <Text className={styles.label()}>{label}</Text>
       <Pressable className={styles.wrapper()}>
-        {leftIcon && <Ionicons name={leftIcon} size={22} />}
+        <Ionicons name='person' size={22} />
         <TextInput className={styles.input()} {...rest} />
 
         <TouchableOpacity>
-          {rightIcon && <Ionicons name={rightIcon} size={22} />}
+          <Ionicons name='eye-off-outline' size={22} />
         </TouchableOpacity>
       </Pressable>
     </View>
---
+++ src/components/AppInput/useAppInputViewModel.ts
@@ -0,0 +1,67 @@
+import { useRef, useState } from 'react'
+import { BlurEvent, FocusEvent, TextInput } from 'react-native'
+
+import { colors } from '../../styles/colors'
+
+interface AppInputViewModelProps {
+  isError?: boolean
+  isDisable?: boolean
+  error?: string
+  secureTextEntry?: boolean
+  onFocus?: (event: FocusEvent) => void
+  onBlur?: (event: BlurEvent) => void
+  mask?: (text: string) => string | undefined
+  onChangeText?: (text: string) => string | void
+  value?: string
+}
+
+export const useAppInputViewModel = ({
+  isError,
+  isDisable,
+  error,
+  secureTextEntry,
+  onFocus,
+  onBlur,
+  mask,
+  onChangeText,
+  value,
+}: AppInputViewModelProps) => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [isFocused, setIsFocused] = useState(false)
+
+  const inputRef = useRef<TextInput>(null)
+
+  const handlePasswordToggle = () => {
+    setShowPassword((prevValue) => !prevValue)
+  }
+
+  const handleWrapperPress = () => {
+    inputRef.current?.focus()
+  }
+
+  const handleFocus = (event: FocusEvent) => {
+    setIsFocused(true)
+    onFocus?.(event)
+  }
+
+  const handleBlur = (event: BlurEvent) => {
+    setIsFocused(false)
+    onBlur?.(event)
+  }
+
+  const getIconColor = () => {
+    if (isFocused) return colors['purple-base']
+    if (isError) return colors.danger
+    if (value) return colors['purple-base']
+    return colors.gray[200]
+  }
+
+  return {
+    getIconColor,
+    handleBlur,
+    handleFocus,
+    handleWrapperPress,
+    handlePasswordToggle,
+    showPassword,
+  }
+}
