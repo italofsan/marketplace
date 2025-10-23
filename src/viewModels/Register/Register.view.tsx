da5f5eb4d9e201a7feb84b1a96e8060c2405997a
Italo Santos <italofsan@outlook.com>
2025-10-23 04:30:04 -0300

    Criando KeyboardAvoidingView

--- src/viewModels/Register/Register.view.tsx
+++ src/viewModels/Register/Register.view.tsx
@@ -1,34 +1,67 @@
-import { Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView, Text, TouchableOpacity } from 'react-native'
 import { router } from 'expo-router'
 
-import { useRegisterViewModel } from './useRegister.viewModel'
 import { AppInputController } from '../../shared/components/AppInputController'
+import { KeyboardContainer } from '../../shared/components/ContainerKeyboard'
 import { AuthFormHeader } from '../../shared/components/AuthFormHeader'
+
+import { useRegisterViewModel } from './useRegister.viewModel'
 
 export const RegisterView = ({
   onSubmit,
   control,
 }: ReturnType<typeof useRegisterViewModel>) => {
   return (
-    <View className='flex-1  justify-center'>
-      <AuthFormHeader
-        title='Crie sua conta'
-        subTitle='Informe seus dados pessoais e de acesso'
-      />
-      <AppInputController
-        leftIcon='mail-outline'
-        label='E-MAIL'
-        control={control}
-        name='email'
-      />
+    <KeyboardContainer>
+      <ScrollView className='flex-1 px-[40px]'>
+        <AuthFormHeader
+          title='Crie sua conta'
+          subTitle='Informe seus dados pessoais e de acesso'
+        />
+        <AppInputController
+          leftIcon='person-outline'
+          label='NOME'
+          control={control}
+          name='name'
+        />
 
-      <TouchableOpacity onPress={onSubmit}>
-        <Text>Registrar</Text>
-      </TouchableOpacity>
+        <AppInputController
+          leftIcon='call-outline'
+          label='TELEFONE'
+          control={control}
+          name='phone'
+        />
+        <AppInputController
+          leftIcon='mail-outline'
+          label='E-MAIL'
+          control={control}
+          name='email'
+        />
 
-      <TouchableOpacity onPress={() => router.push('/login')}>
-        <Text>Login</Text>
-      </TouchableOpacity>
-    </View>
+        <AppInputController
+          leftIcon='lock-closed-outline'
+          label='SENHA'
+          control={control}
+          name='password'
+          secureTextEntry
+        />
+
+        <AppInputController
+          leftIcon='lock-closed-outline'
+          label='CONFIRMAR SENHA'
+          control={control}
+          name='confirmPassword'
+          secureTextEntry
+        />
+
+        <TouchableOpacity onPress={onSubmit}>
+          <Text>Registrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push('/login')}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardContainer>
   )
 }
