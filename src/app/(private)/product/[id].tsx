1fb8c96eed01d3d11cad90f58d13c13220c271b5
Italo Santos <italofsan@outlook.com>
2025-11-29 09:50:16 -0300

    Integrando query de detalhes na viewModel

--- src/app/(private)/product/[id].tsx
+++ src/app/(private)/product/[id].tsx
@@ -1,11 +1,11 @@
 import { useLocalSearchParams } from 'expo-router'
-import { Text, View } from 'react-native'
+
+import { useProductViewModel } from '../../../viewModels/Product/useProduct.viewModel'
+import { ProductView } from '../../../viewModels/Product/Product.view'
 
 export default function Product() {
   const { id } = useLocalSearchParams<{ id: string }>()
-  return (
-    <View>
-      <Text>Detalhes do produto: {id}</Text>
-    </View>
-  )
+
+  const viewModel = useProductViewModel(Number(id))
+  return <ProductView {...viewModel} />
 }
---
+++ src/viewModels/Product/Product.view.tsx
@@ -0,0 +1,27 @@
+import { FlatList, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useProductViewModel } from './useProduct.viewModel'
+
+export const ProductView = ({
+  isLoading,
+  productDetails,
+  error,
+}: ReturnType<typeof useProductViewModel>) => {
+  if (error) {
+    return <Text>Houve um erro ao carregar os detalhes do produto</Text>
+  }
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={[]}
+        renderItem={() => <></>}
+        ListHeaderComponent={() => (
+          <>
+            <Text>{productDetails?.name}</Text>
+          </>
+        )}
+      />
+    </SafeAreaView>
+  )
+}
