349fd576af20681b07f0f8b4454e815748eb5c8b
Italo Santos <italofsan@outlook.com>
2025-11-28 21:11:26 -0300

    Montando base do componente de filtragem de produtos

--- src/viewModels/Home/components/Filter/Filter.view.tsx
+++ src/viewModels/Home/components/Filter/Filter.view.tsx
@@ -1,6 +1,54 @@
-import { View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+
+import { AppButton } from '../../../../shared/components/AppButton'
+import { AppInput } from '../../../../shared/components/AppInput'
+import { colors } from '../../../../styles/colors'
+
 import { useFilterViewModel } from './useFilter.viewModel'
 
 export const FilterView = ({}: ReturnType<typeof useFilterViewModel>) => {
-  return <View></View>
+  return (
+    <View>
+      <View className='flex-row items-center justify-between py-4 px-6'>
+        <Text className='text-lg font-bold text-gray-900'>
+          Filtrar anúncios
+        </Text>
+        <TouchableOpacity>
+          <Ionicons name='close' size={20} color={colors['purple-base']} />
+        </TouchableOpacity>
+      </View>
+
+      <View className='py-4 px-6'>
+        <Text className='font-semibold text-base text-gray-300'>VALOR</Text>
+        <View className='flex-row mb-4 w-full'>
+          <View className='flex-1'>
+            <AppInput
+              placeholder='De'
+              keyboardType='numeric'
+              containerClassName='w-[90%]'
+            />
+          </View>
+          <View className='flex-1'>
+            <AppInput
+              placeholder='Até'
+              keyboardType='numeric'
+              containerClassName='w-[90%]'
+            />
+          </View>
+        </View>
+
+        <Text className='font-semibold text-base text-gray-300'>CATEGORIA</Text>
+
+        <View className='flex-row gap-3 mt-4 mb-6'>
+          <View className='flex-1'>
+            <AppButton variant='outlined'>Limpar filtro</AppButton>
+          </View>
+          <View className='flex-1'>
+            <AppButton>Filtrar</AppButton>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
 }
